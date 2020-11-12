3f56e6c1d3cf5e8e2e1d101078f43c59ec0f91fc
kumari-nishu <kn2492@columbia.edu>
2020-11-12 17:07:01 +0000

    Adding get_cifar utility for 10 class

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -11,6 +11,30 @@ from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 from scipy.stats import norm
 
+def get_cifar_dataset(train = True):
+  transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+  transform_test = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+  ])
+
+  transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+  if train == True:
+      dataset = datasets.CIFAR10(root = './data', train = train, transform=transform, target_transform=None, download=True)
+  else:
+      dataset = datasets.CIFAR10(root = './data', train = train, transform=transform, target_transform=None, download=True)
+
+  return dataset
 
 def get_cifar100_dataset(train = True):
   transform_train = transforms.Compose([
